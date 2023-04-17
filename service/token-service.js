1a6a9b289b2efd0a3551680e8c426b1ef7ddff02
Kobra <programirovanie2003@yandex.ru>
2023-04-17 13:45:52 +0300

    Смена жизни access токена с 30 секунд на 30 минут

--- service/token-service.js
+++ service/token-service.js
@@ -3,7 +3,7 @@ const TokenModel = require('../models/token-model')
 
 class TokenService {
     generateToken(payLoad) {
-        const accessToken = jwt.sign(payLoad, process.env.JWT_ACCESS_SECRET, { expiresIn: '30s' });
+        const accessToken = jwt.sign(payLoad, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' });
         const refreshToken = jwt.sign(payLoad, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' });
         return {
             accessToken,
